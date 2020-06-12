37d8135315efbce0fa63669d9f09de8ad2166eba
evan <aspaasevan@gmail.com>
2020-06-12 08:49:48 -0700

    2 tests passing on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
+# Non-growable buffer with a fixed size
+# When the ring buffer is full and a new element is inserted
+    # oldest element in the ring buffer is overwritten with the newest element 
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = list()
 
     def append(self, item):
-        pass
+        # for item in range(capacity)
+        # or 
+        # while len(storage) < capacity
+        if item not in self.storage: 
+            self.storage.append(item)
+            return True
+        else: 
+            return False
 
     def get(self):
-        pass
+        return self.storage
